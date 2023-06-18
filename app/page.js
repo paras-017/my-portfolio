e0d7ad1137eb456e10ac065c9132b3e33c80aa69
paras-017 <paraschauhan0017@gmail.com>
2023-06-18 22:10:35 +0530

    home page complete

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 'use client'
 import { useEffect } from 'react';
 import canvasDots from './heroCanvas.js';
+import Hero from '@/components/Hero.jsx';
 
 const Home = ()=> {
-  
+
   window.onload = function () {
     canvasDots();
   };
@@ -14,16 +15,7 @@ const Home = ()=> {
         <div className="canvas">
           <canvas className="connecting-dots"></canvas>
         </div>
-        {/* hero */}
-        <div className="heading">
-        <div className="heading__line-1">Hello, I'm <span>Ben</span>.</div>
-        <div className="heading__line-2">I'm a full stack web developer.</div>
-        <a href="#about" className="heading__link">
-          <div className="heading-cta">
-            View my work
-          </div>
-        </a>
-      </div>
+         <Hero/>
       </div>     
     </>
   )
